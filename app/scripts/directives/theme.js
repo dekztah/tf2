3f832560ae3880456c12cabc8ac58ae57fef03f8
Peter Csikos <stuff@wavetrip.org>
2015-04-25 01:05:11 +0200

    Theme changer fix

--- app/scripts/directives/theme.js
+++ app/scripts/directives/theme.js
@@ -5,14 +5,10 @@ angular.module('tf2App').directive('theme', function (localStorageService) {
         restrict: 'A',
         link: function($scope) {
 
-            $scope.theme = localStorageService.get('theme') || {bgr: 'default', color: 'light'};
+            $scope.theme = localStorageService.get('theme') || {white: false};
 
-            $scope.changeTheme = function(theme, scheme) {
-                if (scheme) {
-                    $scope.theme.color = theme;
-                } else {
-                    $scope.theme.bgr = theme;
-                }
+            $scope.changeTheme = function(theme) {
+                $scope.theme.white = theme;
                 localStorageService.set('theme', $scope.theme);
             };
         }
