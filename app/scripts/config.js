aa7c5bfd9efd28000c46d3884b2991c46739f47c
Peter Csikos <stuff@wavetrip.org>
2017-10-06 14:10:16 +0200

    ... space ...

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tf2App').constant({
-    defaultFeeds: [[0,'andrewberg'],[1,'otakugangsta'],[2,'sid766'],[3,'bravecadet'], [4, 'enigmaticsparrow'], [5, 'wiremuse'],[6, '4nt1r34l']]
+    defaultFeeds: [[0,'andrewberg'],[1,'otakugangsta'],[2,'sid766'],[3,'bravecadet'], [4, 'enigmaticsparrow'], [5, 'wiremuse'], [6, '4nt1r34l']]
 });
 
 angular.module('tf2App').config(function (localStorageServiceProvider) {
