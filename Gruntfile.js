6b4faf00a1628ee421cc6dfbdfaed36d299961e4
Peter Csikos <stuff@wavetrip.org>
2018-11-30 13:22:34 +0100

    Make it run in 2018...

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,8 @@ module.exports = function (grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
+    const sass = require('node-sass');
+
     // Configurable paths for the application
     var appConfig = {
         app: require('./bower.json').appPath || 'app',
@@ -208,6 +210,7 @@ module.exports = function (grunt) {
 
         sass: {
             options: {
+                implementation: sass,
                 sourceMap: true
             },
             dist: {
